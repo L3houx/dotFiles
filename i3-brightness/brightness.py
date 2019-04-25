4692197fede66df27255eb37b5a9e9ce22ab0153
L3houx <felix.lehoux@hotmail.com>
2019-04-25 16:52:48 -0400

    Update i3 config and the brightness is binding to keyboard keys.

--- i3-brightness/brightness.py
+++ i3-brightness/brightness.py
@@ -7,26 +7,27 @@ max_brightness = float(open(brightness_path + "max_brightness", "r").readline().
 
 brightness_pourcentage = current_brightness / max_brightness
 
+def information():
+	print "Insert the parameter '+' or '-' to increase or decrease the brightness"
+	print "Example: python brightness.py + to increase the brightness by 10%"
+	print "Example: python brightness.py - to decrease the brightness by 10%"
+
 if len(sys.argv) != 2:
-    print "Insert the parameter '+' or '-' to increase or decrease the brightness"
-    exit()
+	information()
+	exit()
 else:
-    if sys.argv[1] == "+":
-        if current_brightness != max_brightness:
-            print "+"
-            print current_brightness / max_brightness
-            file_brightness = open(brightness_path + "brightness","w") 
-            print current_brightness
-            brightness_upgraded = int(current_brightness + (max_brightness * 0.10))
-            file_brightness.write(str(brightness_upgraded))
-    elif sys.argv[1] == "-":
-        if current_brightness > 0:
-            print "-"
-            print current_brightness / max_brightness
-            file_brightness = open(brightness_path + "brightness","w") 
-            print current_brightness
-            brightness_upgraded = int(current_brightness - (max_brightness * 0.10))
-            file_brightness.write(str(brightness_upgraded))
-    else:
-        print "Insert the parameter '+' or '-' to increase or decrease the brightness"
-        exit()
+	if sys.argv[1] == "+" and current_brightness != max_brightness:
+		print "+"
+		file_brightness = open(brightness_path + "brightness","w")
+		brightness_upgraded = int(current_brightness + (max_brightness * 0.10))
+		print brightness_upgraded / max_brightness
+		file_brightness.write(str(brightness_upgraded))
+	elif sys.argv[1] == "-" and current_brightness > 0:
+		print "-"
+		file_brightness = open(brightness_path + "brightness","w")
+		brightness_upgraded = int(current_brightness - (max_brightness * 0.10))
+		print brightness_upgraded / max_brightness
+		file_brightness.write(str(brightness_upgraded))
+	else:
+		information()
+		exit()
